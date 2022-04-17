4f88b0085c88abb7fccc5c0bbe757734efd75f9a
Hannu Parviainen <hpparvi@gmail.com>
2022-04-17 13:49:23 +0100

    Updated ExternalDataLPF.

--- src/externaldatalpf.py
+++ src/externaldatalpf.py
@@ -4,7 +4,7 @@ import seaborn as sb
 from pathlib import Path
 
 from matplotlib.pyplot import setp, subplots
-from numpy import radians, argsort, percentile, median, array, ones, zeros, arange, concatenate
+from numpy import radians, argsort, percentile, median, array, ones, zeros, arange, concatenate, squeeze
 from numpy.random import permutation
 
 from pytransit import LinearModelBaseline, NormalPrior as NP
@@ -15,7 +15,7 @@ from pytransit.orbits import epoch, fold
 
 from src.dataimport import load_tess, load_beatty_2017, load_spitzer, load_croll_2015
 from src.kelt1 import (zero_epoch, period, filter_names, beaming_amplitudes as ba, beaming_uncertainties as be,
-                       ev_amplitudes as eva,)
+                       ev_amplitudes as eva, star_rho, )
 
 ds = dict(lbt=2., croll=2., spitzer=2., tess=None)
 
@@ -32,7 +32,7 @@ class ExternalDataLPF(PhaseCurveLPF):
           d) Unconstrained emission and reflection + theoretical ev
         """
         self.scenarios = 'a b c d'.split()
-        self.labels = {'a': 'emission_and theoretical_ev',
+        self.labels = {'a': 'emission_and_theoretical_ev',
                        'b': 'emission_and_constrained_ev',
                        'c': 'emission_and_unconstrained_ev',
                        'd': 'emission_and_reflection_and_theoretical_ev'}
@@ -74,7 +74,7 @@ class ExternalDataLPF(PhaseCurveLPF):
         super()._post_initialisation()
         self.set_prior('tc', 'NP', zero_epoch.n, 0.01)   # - Zero epoch: normal prior with an inflated uncertainty
         self.set_prior('p', 'NP', period.n, 3*period.s)  # - Orbital period: normal prior with an inflated uncertainty
-        self.set_prior('rho', 'NP', 0.6, 0.075)          # - Stellar density: wide normal prior
+        self.set_prior('rho', 'NP', star_rho.n, 3*star_rho.s)          # - Stellar density: wide normal prior
         self.set_prior('secw', 'NP', 0.0, 1e-6)          # - Circular orbit: sqrt(e) cos(w) and sqrt(e) sin(w) forced
         self.set_prior('sesw', 'NP', 0.0, 1e-6)          #   to zero with normal priors.
         self.set_prior('k2', 'NP', 0.078, 0.005)         # - Area ratio: wide normal prior based on Siverd et al. (2012)
@@ -93,19 +93,19 @@ class ExternalDataLPF(PhaseCurveLPF):
         # ----------------------------------
         # We may modify these later based on the chosen
         # simulation scenario.
-        self.set_prior('oev', 'NP', 0.0, 1e-7)
+        self.set_prior('oev', 'NP', 0.0, 0.09)
         for pb in self.passbands:
             self.set_prior(f'aev_{pb}', 'UP', 0.0, 1000e-6)      # - Ellipsoidal variation
-            self.set_prior(f'ted_{pb}', 'UP', 0.0, 0.5)          # - Emission day-side flux ratio
-            self.set_prior(f'ten_{pb}', 'UP', 0.0, 0.5)          # - Emission night-side flux ratio
+            self.set_prior(f'log10_ted_{pb}', 'UP', -3.0, 0.0)   # - Emission dayside flux ratio
+            self.set_prior(f'log10_ten_{pb}', 'UP', -4.0, 0.0)   # - Emission nightside flux ratio
             self.set_prior(f'teo_{pb}', 'NP', 0.0, radians(10))  # - Emission peak offset
 
         # Force the H and Ks band emission offset to zero
         # -----------------------------------------------
         # We do this because we don't have enough phase
         # coverage to measure the offsets.
-        self.set_prior('teo_H', 'NP', 0.0, 1e-5)
-        self.set_prior('teo_Ks', 'NP', 0.0, 1e-5)
+        #self.set_prior('teo_H', 'NP', 0.0, 1e-5)
+        #self.set_prior('teo_Ks', 'NP', 0.0, 1e-5)
 
         # Set priors on the Doppler beaming amplitudes
         # --------------------------------------------
@@ -113,9 +113,9 @@ class ExternalDataLPF(PhaseCurveLPF):
         # BT-Settl model spectra for KELT-1 (see Appendix 2
         # notebook), and vary from 65 ppm in the CHEOPS passband
         # to 15 ppm in the Spitzer passbands.
-        self.set_prior('adb_TESS', 'NP', ba['TESS'], 2*be['TESS'])
-        self.set_prior('adb_H',    'NP', ba['H'],    2*be['H'])
-        self.set_prior('adb_Ks',   'NP', ba['Ks'],   2*be['Ks'])
+        self.set_prior('adb_TESS', 'NP', ba['TESS'],   2*be['TESS'])
+        self.set_prior('adb_H',    'NP', ba['H'],      2*be['H'])
+        self.set_prior('adb_Ks',   'NP', ba['Ks'],     2*be['Ks'])
         self.set_prior('adb_36um', 'NP', ba['36um'],   2*be['36um'])
         self.set_prior('adb_45um', 'NP', ba['45um'],   2*be['45um'])
 
@@ -139,10 +139,10 @@ class ExternalDataLPF(PhaseCurveLPF):
             for pb in self.passbands:
                 self.set_prior(f'aev_{pb}', 'NP', eva[pb].n, eva[pb].s)
         elif self.scenario == "b":
-            pr_e_h = NP(0.81, 0.05)
-            pr_e_ks = NP(0.80, 0.05)
-            pr_e_s1 = NP(0.77, 0.10)
-            pr_e_s2 = NP(0.77, 0.10)
+            pr_e_h = NP(0.81, 0.01)
+            pr_e_ks = NP(0.80, 0.01)
+            pr_e_s1 = NP(0.77, 0.02)
+            pr_e_s2 = NP(0.77, 0.02)
             def ev_amplitude_prior(pvp):
                 h_ratio = pvp[:, 14] / pvp[:, 8]
                 ks_ratio = pvp[:, 20] / pvp[:, 8]
@@ -153,6 +153,18 @@ class ExternalDataLPF(PhaseCurveLPF):
         elif self.scenario == 'c':
             pass
 
+        # Force hot spot offsets close to similar values
+        # ----------------------------------------------
+        pr_emission_offset_difference = NP(0.0, 0.07)
+        def emission_offset(pvp):
+            eo_mean = pvp[:, [12, 18, 24, 30, 36]].mean()
+            return (  pr_emission_offset_difference.logpdf(pvp[:, 12] - eo_mean)
+                    + pr_emission_offset_difference.logpdf(pvp[:, 18] - eo_mean)
+                    + pr_emission_offset_difference.logpdf(pvp[:, 24] - eo_mean)
+                    + pr_emission_offset_difference.logpdf(pvp[:, 30] - eo_mean)
+                    + pr_emission_offset_difference.logpdf(pvp[:, 36] - eo_mean))
+        self.add_prior(emission_offset)
+
     # Define the log likelihood
     # -------------------------
     # We use a GP log likelihood for the TESS, LBT, and CFHT data and
@@ -162,6 +174,10 @@ class ExternalDataLPF(PhaseCurveLPF):
             self._add_lnlikelihood_model(CeleriteLogLikelihood(self, noise_ids=[1]))  # LBT H
             self._add_lnlikelihood_model(CeleriteLogLikelihood(self, noise_ids=[2]))  # CFHT Ks
             self._add_lnlikelihood_model(WNLogLikelihood(self, noise_ids=array([3,4,5,6])))
+
+    def lnposterior(self, pv):
+        return squeeze(super().lnposterior(pv))
+
 
 def plot_joint_lcs(lpf, method='de', pv=None):
     yoff = 5e-3
