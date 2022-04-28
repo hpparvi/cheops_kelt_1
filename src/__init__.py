18833b804db59b203c0ac454d38aec36a017c038
Hannu Parviainen <hpparvi@gmail.com>
2022-04-28 15:15:46 +0100

    Updated __init__.py.

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
-from .kelt1 import read_mcmc
+from .kelt1 import read_mcmc, AAPGW, AAOCW
 from .externaldatalpf import ExternalDataLPF
 from .cheopslpf import CHEOPSLPF
 from .finallpf import FinalLPF
